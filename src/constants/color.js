5180e407173cd43f27306e41440fc0ad19bac4fd
Sourin Ghanty <souringhanty.20@gmail.com>
2025-01-14 01:31:52 +0530

    Changes on color theme

--- src/constants/color.js
+++ src/constants/color.js
@@ -4,7 +4,7 @@ export const orangeLight = "rgba(234, 112, 112,0.2)";
 export const grayColor = "rgb(231, 219, 219)";
 export const lightBlue = "#2694ab";
 export const matBlack = "#1c1c1c";
-export const bgGradient = "linear-gradient(rgb(255 225 209), rgb(249 159 159))";
+export const bgGradient = "linear-gradient(90deg, rgba(254,255,250,0.40948879551820727) 0%, rgba(89,239,220,0.7456232492997199) 10%, rgba(20,86,31,0.510329131652661) 100%)";
 
 export const purple = "rgba(75,12,192,1)";
 export const purpleLight = "rgba(75,12,192,0.2)";
